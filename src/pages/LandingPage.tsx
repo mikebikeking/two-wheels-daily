cf37c062bbff06e3b6f284b465be2841ecb15703
Michael King <131377617+mikebikeking@users.noreply.github.com>
2026-02-04 11:58:37 -0500

    remove tests

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { HeroSection } from '../components/HeroSection';
 import { BentoGrid } from '../components/BentoGrid';
-import { Timeline } from '../components/Timeline';
-import { Testimonials } from '../components/Testimonials';
 import { FinalCTA } from '../components/FinalCTA';
 interface LandingPageProps {
   onOpenModal: () => void;
@@ -12,8 +10,6 @@ export function LandingPage({ onOpenModal }: LandingPageProps) {
     <main className="min-h-screen bg-[#050505] text-white selection:bg-[#DFFF00] selection:text-black">
       <HeroSection />
       <BentoGrid />
-      <Timeline />
-      <Testimonials />
       <FinalCTA onOpenModal={onOpenModal} />
 
       {/* Simple Footer */}
